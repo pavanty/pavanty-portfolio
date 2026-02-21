9ab3f3f73c5f07d50ebeb1cb06a5cb49a9bb01b9
Pavan T Y <typavan916@gmail.com>
2026-02-21 04:27:59 -0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -7,31 +7,23 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Certifications from "./components/Certifications/Certifications";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
+    const timer = setTimeout(() => upadateLoad(false), 1200);
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <Router basename="/pavanty-portfolio">
+    <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
