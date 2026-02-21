0bc5199dd006e51de2f0407a810b08d056684627
Pavan T Y <typavan916@gmail.com>
2026-02-21 04:32:46 -0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
@@ -7,7 +8,13 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Certifications from "./components/Certifications/Certifications";
 
-import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 import ScrollToTop from "./components/ScrollToTop";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,26 +22,33 @@ import "./style.css";
 import "./App.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => upadateLoad(false), 1200);
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <Router>
+    <Router basename="/pavanty-portfolio">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/certifications" element={<Certifications />} />
+
+          {/* Fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
+
         <Footer />
       </div>
     </Router>
